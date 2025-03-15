71db4dd274d89b28b8872a4394a0260e7082ac35
Asadullah325 <asadullah456545@gmail.com>
2025-03-15 19:45:00 +0500

    settings

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -127,3 +127,16 @@ export const logoutUser = async (req, res) => {
         res.status(500).json({ message: 'Server Error' });
     }
 };
+
+export const getUserProfile = async (req, res) => {
+    try {
+        const user = await User.findById(req.params.id);
+        if (!user) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+        res.status(200).json(user);
+    } catch (error) {
+        console.error("Get User Profile Error:", error.message);
+        res.status(500).json({ message: 'Server Error' });  
+    }
+}
--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,10 +1,12 @@
 import express from 'express';
-import { loginUser, logoutUser, registerUser } from '../controllers/user.controller.js';
+import { getUserProfile, loginUser, logoutUser, registerUser } from '../controllers/user.controller.js';
 
 const router = express.Router();
 
 router.route('/register').post(registerUser);
 router.route('/login').post(loginUser);
 router.route('/logout').get(logoutUser);
+router.route("/profile/:id").get(getUserProfile);
 
 export default router;
+
